82a36bb1d3002dc16b75cf37d3783aac581cb2c1
Denine Guy <denineguy@gmail.com>
2014-09-25 18:13:18 -0400

    User model and testing set up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @user = User.new(username: "Lisa Example", email: "lisa@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.username = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " "}
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.username = "a" * 51 }
+    it { should_not be_valid }
+  end
 end
