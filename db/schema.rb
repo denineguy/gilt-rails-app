b26c3c0f0a810101102cc25ffd3acac39bde0f9b
Denine Guy <denineguy@gmail.com>
2014-09-29 13:35:07 -0400

    added functionality to confirm a valid user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925215010) do
+ActiveRecord::Schema.define(version: 20140929172702) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20140925215010) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
