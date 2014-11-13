00802727eabd0934b4512f4b7a2bc3bf65a83dcd
Denine Guy <denineguy@gmail.com>
2014-11-12 23:47:13 -0500

    cleaned up code in home page

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
   var apikey = 'df180f455f59f4441b26c77d27a8727e';
-  // URL = 'https://api.gilt.com/v1/products?q=shoes&store=women&size=WoSh%3A'+ userShoeSize +'&apikey='+ apikey;
   var URL = 'https://api.gilt.com/v1/sales/women/active.json?apikey='+ apikey;
   $.ajax({
     url: URL,
@@ -11,47 +10,30 @@ $(document).ready(function(){
       console.log("Gilt API Successful");
       console.log(response.sales);
       var imageArray = [];
-      var index = Math.floor((Math.random() * 10) + 1);
-      for(var i=0; i<20; i++){
-        // var product_name = response.sales[i].description;
-        // for(var j=0; j<response.sales[i].image_urls.length; j++){
-        //     var image_object = response.sales[i].image_urls[j];
-        //     console.log(image_object);
-        // }
-        // for(var key in image_object){
-        //   var image = image_object[key].url;
-        //   console.log(image);
-        // }
-       
+      var responseLength = response.sales.length;
 
+      for(var i=0; i<responseLength; i++){
+        
         for (var key in response.sales[i].image_urls) {
           var image_object = response.sales[i].image_urls[key];
-          console.log(image_object);
+          // console.log(image_object);
         }  
+
         for (var key2 in image_object) {
             var image = image_object[key2].url;
             imageArray.push(image);
-            console.log(imageArray[11] + index);
-            var number = Math.floor((Math.random() * 10) + 1);
-            var heroImage = imageArray[number];
-            console.log(index);
-            console.log(heroImage);
-            console.log(image);
+            
+            var index = Math.floor((Math.random() * responseLength) + 1);
+            var heroImage = imageArray[index];
         }
 
-        // for (var key2 in image_object) {
-        // var image = image_object[key2].url;
-        // console.log(key2);
-        // }
-        // $('#welcome-page').append("<div class='home-image-container'><div class='hero-image-container'><div class='hero_info' style='background-image:url(" + imageArray[0] + ");background-size:cover'></div></div>" +
-                              // "</ul></div></div>");
-        // $('.home-page-hero').append("<li>"+product_name+"</li>");
-        // $('#welcome-page, h6').fadeIn(3000);
       }
-      $('#welcome-page').append("<div class='home-image-container'><div class='hero-image-container'><div class='hero_info' style='background-image:url(" + imageArray[index] + ");background-size:cover'></div></div>" +
-                              "</ul></div></div>");
+      $('#welcome-page').append("<div class='home-image-container'><div class='hero-image-container'>"+
+            "<div class='hero_info' style='background-image:url(" + heroImage + ")"+
+            ";background-size:cover'></div></div></ul></div></div>");
         // $('.home-page-hero').append("<li>"+product_name+"</li>");
-        $('#welcome-page, h6').fadeIn(3000);
+      $('#welcome-page, h6').fadeIn(3000);
+
       $('.buy-container').hide();
      
       $('.feed-container').hover(function(){
